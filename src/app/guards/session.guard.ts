a455b4982f06cf054d947392ad2432dcabd56f82
Kanin-020 <l2001jmigue@hotmail.com>
2023-10-16 21:00:17 -0600

    Update session.guard.ts

--- src/app/guards/session.guard.ts
+++ src/app/guards/session.guard.ts
@@ -10,19 +10,18 @@ export class SessionGuard implements CanActivate {
         const token = localStorage.getItem('token');
 
         if (token) {
-            // Token presente en el localStorage
+
             console.log('Autenticado');
-            return true; // Permite el acceso a la ruta
+            return true;
         } else {
             // No hay un token en el localStorage, el usuario no está autenticado
             alert('No hay una sesión válida');
-            this.router.navigate(['/login']); // Redirecciona a la página de inicio de sesión
-            return false; // Bloquea el acceso a la ruta
+            this.router.navigate(['/login']);
+            return false;
         }
     }
 
     logout(): void {
-        // Agrega una función para cerrar sesión que elimine el token del localStorage
         localStorage.removeItem('token');
         this.router.navigate(['/login']);
     }
