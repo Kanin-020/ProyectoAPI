3e4c2f4e9c27265fdcb39db9123f9a272126cd8d
Kanin-020 <l2001jmigue@hotmail.com>
2023-10-24 23:46:22 -0600

    Comentarios agregados

--- src/app/components/user/taskDetails/taskDetails.component.ts
+++ src/app/components/user/taskDetails/taskDetails.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
+import { SessionGuard } from 'src/app/guards/session.guard';
+import * as commentListJSON from "src/assets/json/commentSample.json";
+
 
 @Component({
   selector: 'app-taskDetails',
   templateUrl: './taskDetails.component.html',
-  styleUrls: ['./taskDetails.component.scss']
+  styleUrls: ['./taskDetails.component.scss'],
+  providers: [SessionGuard]
 })
 export class TaskDetailsComponent {
+  protected commentList: any[] = [];
 
+  protected page: PageEvent = {
+    length: 0,
+    pageIndex: 0,
+    pageSize: 10,
+    previousPageIndex: 0
+  };
+
+  constructor() { }
+
+
+  ngOnInit() {
+    this.commentList = (commentListJSON as any).default;
+    this.page.length = this.commentList.length;
+
+    console.log(this.commentList.length);
+  }
 }
