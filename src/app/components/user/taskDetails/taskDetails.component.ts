0d8d3cc6cb121b402a5412940853b06cf6ff336b
Error72890 <carlos72890@gmail.com>
2023-11-13 17:15:58 -0600

    Más cosas de user

--- src/app/components/user/taskDetails/taskDetails.component.ts
+++ src/app/components/user/taskDetails/taskDetails.component.ts
@@ -5,32 +5,35 @@ import { ActivatedRoute } from '@angular/router';
 import { TaskService } from 'src/app/services/api/task.service';
 import { ProjectService } from 'src/app/services/api/project.service';
 import { CommentService } from 'src/app/services/api/comment.service';
-import { Project, Task, Comment, User, Relation } from 'src/app/interfaces/interfaces';
+import {
+  Project,
+  Task,
+  Comment,
+  User,
+  Relation,
+} from 'src/app/interfaces/interfaces';
 import { UserService } from 'src/app/services/api/user.service';
 import { forkJoin } from 'rxjs';
 import { RelationsCommentsService } from 'src/app/services/api/relationsComments.service';
 import { RelationsProjectsService } from 'src/app/services/api/relationsProjects.service';
 
-
 @Component({
   selector: 'app-taskDetails',
   templateUrl: './taskDetails.component.html',
   styleUrls: ['./taskDetails.component.scss'],
-  providers: [SessionGuard]
+  providers: [SessionGuard],
 })
 export class TaskDetailsComponent {
-
   page: PageEvent = {
     length: 0,
     pageIndex: 0,
     pageSize: 10,
-    previousPageIndex: 0
+    previousPageIndex: 0,
   };
 
   workersList: User[] = [];
-  workerNameList: string[] = [];
-  commentOwnList: Comment[] = [];
-  commentOtherList: Comment[] = [];
+  assignedWorkersList: string[] = [];
+  commentList: Comment[] = [];
 
   relationsCommentsList: Relation[] = [];
   relationsProjectsList: Relation[] = [];
@@ -68,147 +71,141 @@ export class TaskDetailsComponent {
     date: '',
   };
 
-  constructor(private route: ActivatedRoute, private taskService: TaskService, private projectService: ProjectService, private commentService: CommentService, private userService: UserService, private relationsCommentsService: RelationsCommentsService, private relationsProjectsService: RelationsProjectsService) {
-    this.route.params.subscribe(params => {
+  constructor(
+    private route: ActivatedRoute,
+    private taskService: TaskService,
+    private projectService: ProjectService,
+    private commentService: CommentService,
+    private userService: UserService,
+    private relationsCommentsService: RelationsCommentsService,
+    private relationsProjectsService: RelationsProjectsService
+  ) {
+    this.route.params.subscribe((params) => {
       this.taskId = params['taskId'];
     });
   }
 
   async ngOnInit() {
-
-
     forkJoin([
       this.taskService.getTaskById(this.taskId),
       this.userService.getWorkersList(),
       this.relationsCommentsService.getCommentRelationByTaskId(this.taskId),
       this.relationsProjectsService.getProjectRelationByTaskId(this.taskId),
-    ]).subscribe(async ([taskResponse, workerResponse, relationCommentsResponse, relationProjectsResponse]) => {
+    ]).subscribe(
+      async ([
+        taskResponse,
+        workerResponse,
+        relationCommentsResponse,
+        relationProjectsResponse,
+      ]) => {
+        const taskItem: Task = await (taskResponse as any).task;
+        const workersList: User[] = await (workerResponse as any).workers;
+        const relationCommentsList: Relation[] = await (
+          relationCommentsResponse as any
+        ).relations_comments;
+        const relationProjectsList: Relation[] = await (
+          relationProjectsResponse as any
+        ).relations_projects;
 
-      const taskItem: Task = await (taskResponse as any).task;
-      const workersList: User[] = await (workerResponse as any).workers;
-      const relationCommentsList: Relation[] = await (relationCommentsResponse as any).relations_comments;
-      const relationProjectsList: Relation[] = await (relationProjectsResponse as any).relations_projects;
+        this.taskItem = taskItem;
+        this.workersList = workersList;
+        this.relationsCommentsList = relationCommentsList;
+        this.relationsProjectsList = relationProjectsList;
 
-      this.taskItem = taskItem;
-      this.workersList = workersList;
-      this.relationsCommentsList = relationCommentsList;
-      this.relationsProjectsList = relationProjectsList;
-
-      this.getProjectData();
-      this.getCommentsData();
-
-    });
-
-
-
+        this.getProjectData();
+        this.getCommentsData();
+      }
+    );
   }
 
-
   getProjectData() {
-
-    this.relationsProjectsList.forEach(projectRelation => {
+    this.relationsProjectsList.forEach((projectRelation) => {
       if (projectRelation.taskId == this.taskId) {
         const projectId = projectRelation.projectId;
 
-        this.projectService.getProjectById(projectId!).subscribe(async (response: any) => {
-          const projectData = response.project;
-          this.projectItem = projectData;
+        this.projectService
+          .getProjectById(projectId!)
+          .subscribe(async (response: any) => {
+            const projectData = response.project;
+            this.projectItem = projectData;
+          });
+        this.workersList.forEach((worker) => {
+          if (projectRelation.userId == worker.userId) {
+            this.assignedWorkersList.push(worker.name);
+          }
         });
       }
     });
-
   }
-
 
   getCommentsData() {
-
-    this.relationsCommentsList.forEach(commentRelation => {
-
+    this.relationsCommentsList.forEach((commentRelation) => {
       if (commentRelation.taskId == this.taskId) {
-
         const commentId = commentRelation.commentId;
-
         const userId = commentRelation.userId;
 
-        this.commentService.getCommentById(commentId!).subscribe(async (response: any) => {
-
-          let commentData = response.comment;
-
-          commentData.userId = userId;
-
-          if (userId == this.userId) {
-            this.commentOwnList.push(commentData);
-          } else {
-            this.commentOtherList.push(commentData);
-          }
-
-        });
-
+        this.commentService
+          .getCommentById(commentId!)
+          .subscribe(async (response: any) => {
+            let commentData = response.comment;
+            commentData.userId = userId;
+            this.commentList.push(commentData);
+          });
       }
-
-      this.workersList.forEach(worker => {
-        if (commentRelation.userId = worker.userId) {
-          this.workerNameList.push(worker.name);
-        }
-      });
-
-      this.workerNameList = this.deleteDuplicatedWorkers(this.workerNameList);
-
     });
-
   }
 
-  deleteDuplicatedWorkers(workers: string[]): string[] {
-
-    const uniqueSet = new Set(workers);
-
-    const uniqueArray = Array.from(uniqueSet);
-
-    return uniqueArray;
+  getUserName(searchUserId: number): string {
+    var name = '';
+    this.workersList.forEach((worker) => {
+      if (worker.userId == searchUserId) {
+        name = worker.name;
+      }
+    });
+    return name;
   }
 
   markAsDone() {
-
     const { name, description, creationDate, deadline } = this.taskItem;
 
-    this.taskService.editTask(this.taskId, name, description, 'Pendiente', creationDate, deadline).subscribe((response: any) => {
-      alert(response.response || response.error);
-    });
-
+    this.taskService
+      .editTask(
+        this.taskId,
+        name,
+        description,
+        'Finalizado',
+        creationDate,
+        deadline
+      )
+      .subscribe((response: any) => {
+        alert(response.response || response.error);
+      });
   }
 
   markAsNotDone() {
-
     const { name, description, creationDate, deadline } = this.taskItem;
 
-    this.taskService.editTask(this.taskId, name, description, 'Activo', creationDate, deadline).subscribe((response: any) => {
-      alert(response.response || response.error);
-    });
-
-  }
-
-  updateComment(comment: Comment, newCommentContent: string) {
-
-    const { commentId, status } = comment;
-
-    this.commentService.editComment(commentId, status, newCommentContent).subscribe(async (response: any) => {
-
-      alert(response.response || response.error);
-
-    });
-
+    this.taskService
+      .editTask(
+        this.taskId,
+        name,
+        description,
+        'Activo',
+        creationDate,
+        deadline
+      )
+      .subscribe((response: any) => {
+        alert(response.response || response.error);
+      });
   }
 
   deleteComment(comment: Comment) {
-
     const { commentId } = comment;
 
-    this.commentService.deleteComment(commentId).subscribe(async (response: any) => {
-
-      alert(response.response || response.error);
-
-    });
-
+    this.commentService
+      .deleteComment(commentId)
+      .subscribe(async (response: any) => {
+        alert(response.response || response.error);
+      });
   }
-
 }
--- src/app/components/user/userLandpage/userLandpage.component.ts
+++ src/app/components/user/userLandpage/userLandpage.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SessionGuard } from 'src/app/guards/session.guard';
-import { TaskService } from "src/app/services/api/task.service";
-import { ProjectService } from "src/app/services/api/project.service";
+import { TaskService } from 'src/app/services/api/task.service';
+import { ProjectService } from 'src/app/services/api/project.service';
 import { Project, Relation, Task } from 'src/app/interfaces/interfaces';
 import { RelationsProjectsService } from 'src/app/services/api/relationsProjects.service';
 import { forkJoin } from 'rxjs';
@@ -11,11 +11,9 @@ import { forkJoin } from 'rxjs';
   templateUrl: './userLandpage.component.html',
   styleUrls: ['./userLandpage.component.scss'],
 
-  providers: [SessionGuard]
-
+  providers: [SessionGuard],
 })
 export class UserLandpageComponent implements OnInit {
-
   userId: number = parseInt(localStorage.getItem('userId')!);
 
   taskList: Task[] = [];
@@ -27,23 +25,25 @@ export class UserLandpageComponent implements OnInit {
   activeTaskList: Task[] = [];
   pendingTaskList: Task[] = [];
 
-  constructor(private taskService: TaskService, private projectService: ProjectService, private relationsProjectsService: RelationsProjectsService) { }
+  constructor(
+    private taskService: TaskService,
+    private projectService: ProjectService,
+    private relationsProjectsService: RelationsProjectsService
+  ) {}
 
   /**
    * Se obtienen los datos de un archivo JSON.
    */
   async ngOnInit() {
-
     forkJoin([
       this.taskService.getTaskList(),
       this.projectService.getProjectList(),
-      this.relationsProjectsService.getProjectRelationByUserId(this.userId)
+      this.relationsProjectsService.getProjectRelationByUserId(this.userId),
     ]).subscribe(async ([taskResponse, projectResponse, relationsResponse]) => {
-
-
       const taskList: Task[] = await (taskResponse as any).tasks;
       const projectList: Project[] = await (projectResponse as any).projects;
-      const relationsProjectsList: Relation[] = await (relationsResponse as any).relations_projects;
+      const relationsProjectsList: Relation[] = await (relationsResponse as any)
+        .relations_projects;
 
       this.taskList = taskList;
       this.projectList = projectList;
@@ -51,44 +51,52 @@ export class UserLandpageComponent implements OnInit {
 
       this.setProjectToTask();
       this.filterTasksByStatus();
-
     });
-
   }
 
   /**
    * Se realiza la unión de datos para generar las tarjetas correspondientes al usuario.
    */
   setProjectToTask() {
-
     this.relationsProjectsList.forEach((relation: Relation) => {
       this.taskList.forEach((task: Task) => {
         this.projectList.forEach((project: Project) => {
-
-          if (relation.projectId == project.projectId && relation.taskId == task.taskId) {
-
+          if (
+            relation.projectId == project.projectId &&
+            relation.taskId == task.taskId
+          ) {
             task.projectName = project.name;
             this.filteredTaskList.push(task);
-
           }
         });
       });
     });
-
   }
 
   /**
    * Filtra las tareas dependiendo de su status.
    */
   filterTasksByStatus() {
+    const now = new Date();
     this.filteredTaskList.forEach((task: Task) => {
       if (task.status == 'Activo') {
-        this.activeTaskList.push(task);
-      }
-      if (task.status == 'Pendiente') {
-        this.pendingTaskList.push(task);
+        const [fullDate, fullTime] = task.deadline.split(', ');
+        const [day, month, year] = fullDate.split('/');
+        const [hours, minutes, seconds] = fullTime.split(':');
+        const deadline = new Date(
+          +year,
+          +month - 1,
+          +day,
+          +hours,
+          +minutes,
+          +seconds
+        );
+        if (deadline > now) {
+          this.activeTaskList.push(task);
+        } else {
+          this.pendingTaskList.push(task);
+        }
       }
     });
   }
 }
-
