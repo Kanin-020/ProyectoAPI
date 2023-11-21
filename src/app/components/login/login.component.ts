873b030b4be78cb89b38e9a1bbb5735f1a696238
Kanin-020 <l2001jmigue@hotmail.com>
2023-11-20 22:42:10 -0600

    Validación login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -15,6 +15,8 @@ export class LoginComponent implements OnInit {
 
   userData: any = {};
   formItem!: FormGroup;
+  invalidSessionCounter: number = 0;
+  blockedFormFlag: boolean = false;
 
   constructor(private router: Router, private userService: UserService) { }
 
@@ -50,16 +52,64 @@ export class LoginComponent implements OnInit {
         localStorage.setItem('role', response.role);
         localStorage.setItem('token', response.token);
 
-        if (response.role == 'trabajador') {
-          this.router.navigate(['/user-landpage']);
+        if (response.ok == true) {
 
-        } else if (response.role == 'administrador') {
-          this.router.navigate(['/admin-landpage']);
+          const role = response.role;
+
+          this.startSession(role);
+
+          this.invalidSessionCounter = 0;
+
+        } else if (response.ok == false) {
+
+          this.invalidSessionCounter++;
+
         }
 
+        this.setBlockedFormFlag();
+
       }
+
     );
 
+  }
+
+  startSession(role: string) {
+    if (role == 'trabajador') {
+      this.router.navigate(['/user-landpage']);
+
+    } else if (role == 'administrador') {
+      this.router.navigate(['/admin-landpage']);
+    }
+  }
+
+  setBlockedFormFlag() {
+
+    if (this.invalidSessionCounter != 3) {
+      this.blockedFormFlag = false;
+    }
+
+    if (this.invalidSessionCounter == 3) {
+      this.restartForm();
+    }
+
+  }
+
+  async restartForm() {
+
+    alert("Esperar 5s para volver a intentar");
+
+    this.blockedFormFlag = true;
+    this.invalidSessionCounter = 0;
+
+    setTimeout(() => this.enableForm(), 5000);
+
+    await new Promise(resolve => setTimeout(resolve, 5000));
+
+  }
+
+  enableForm() {
+    this.blockedFormFlag = false;
   }
 
 }
